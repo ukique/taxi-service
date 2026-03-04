95b3cd7aef3800220b4dae8f2ae7336feafcaed0
Alexey <tokyogodb@gmail.com>
2026-03-04 15:32:01 +0200

    feat: add GIN server on port :8080

--- cmd/taxi-service/main.go
+++ cmd/taxi-service/main.go
@@ -1,1 +1,14 @@
-package taxi_service
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("fail run server on port 8080:", err)
+	}
+}
