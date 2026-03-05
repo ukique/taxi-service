98257424e7d432a1cf216cbc166a51e13295cd47
Alexey <tokyogodb@gmail.com>
2026-03-05 11:25:23 +0200

    feat: add UserRegister handler

---
+++ internal/features/user/transport/handler.go
@@ -0,0 +1,41 @@
+package transport
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/ukique/taxi-service/internal/features/user/repository"
+	"github.com/ukique/taxi-service/internal/models"
+)
+
+func RegisterUserHandler(conn *pgx.Conn) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var user models.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			log.Println("fail to read JSON body:", err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "fail to read JSON"})
+			return
+		}
+		//validating data
+		if user.Username == "" {
+			log.Println("username can't be empty:")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "username can't be empty"})
+			return
+		}
+		if user.Password == "" {
+			log.Println("password can't be empty:")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "password can't be empty"})
+			return
+		}
+
+		if err := repository.RegisterUser(c.Request.Context(), conn, user.Username, user.Password, user.Email); err != nil {
+			log.Println("fail to register user:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "fail to register user"})
+			return
+		}
+
+		c.IndentedJSON(http.StatusCreated, nil) //вернуть успех
+	}
+}
