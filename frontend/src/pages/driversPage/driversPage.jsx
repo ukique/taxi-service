02a9007196d65e18906342f4da8d7c8244a1c47b
Alex Popov <ukiquee@email.com>
2026-03-29 12:05:20 +0300

    frontend: add add-driver button

--- frontend/src/pages/driversPage/driversPage.jsx
+++ frontend/src/pages/driversPage/driversPage.jsx
@@ -1,10 +1,20 @@
+import "./driversPage.css"
 import Header from "../../components/header/header.jsx";
+import RegisterDriverButton from "../../components/drivers/registerDriver.jsx";
 
-function Drivers(){
-    return(
+function Drivers() {
+    return (
         <>
-            <Header />
+            <div  className="drivers-page">
+            <Header/>
+            <div className="drivers-page-main">
+                <h1>Drivers</h1>
+                <RegisterDriverButton/>
+            </div>
+            <h4>Manage driver profiles and monitor status</h4>
+            </div>
         </>
     )
 }
+
 export default Drivers
