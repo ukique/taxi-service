69435e7f5de235824bf08f033cd0a21b59969187
Alex Popov <ukiquee@email.com>
2026-03-30 10:39:36 +0300

    feat: add ChangeDriverName func

---
+++ internal/features/driver/repository/changeDriverData.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ChangeDriverName(ctx context.Context, conn *pgx.Conn, id int, username string) error {
+	sqlQuery := `
+	UPDATE drivers
+	SET username = $1
+	WHERE id = $2;
+`
+	if _, err := conn.Exec(ctx, sqlQuery, username, id); err != nil {
+		return err
+	}
+	return nil
+}
