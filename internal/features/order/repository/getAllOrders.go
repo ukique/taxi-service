4c4d23c1380124dab89a9b7b574b799fd0316339
Alex Popov <ukiquee@email.com>
2026-04-02 09:33:57 +0300

    feat: add GetAllOrders func

---
+++ internal/features/order/repository/getAllOrders.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/ukique/taxi-service/internal/models"
+)
+
+func GetAllOrders(ctx context.Context, conn *pgx.Conn) ([]models.Order, error) {
+	sqlQuery := `
+  SELECT user_id, driver_id, pickup_lat, pickup_lon, dropout_lat, dropout_lon, status, created_at, updated_at FROM orders;
+`
+	var orders []models.Order
+	rows, err := conn.Query(ctx, sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var o models.Order
+		if err := rows.Scan(&o.UserID, &o.DriverID, &o.PickUpLat, &o.PickUpLon, &o.DropOutLat, &o.DropOutLon, &o.Status, &o.CreatedAt, &o.UpdatedAt); err != nil {
+			return nil, err
+		}
+		orders = append(orders, o)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return orders, nil
+}
--- internal/models/orders.go
+++ internal/models/orders.go
@@ -1,7 +1,17 @@
 package models
 
+import "time"
+
 type Order struct {
-	UserID int `json:"user_id"`
+	UserID     int         `json:"user_id"`
+	DriverID   int         `json:"driver_id"`
+	Status     OrderStatus `json:"status"`
+	PickUpLat  float64     `json:"pick_up_lat"`
+	PickUpLon  float64     `json:"pick_up_lon"`
+	DropOutLat float64     `json:"drop_out_lat"`
+	DropOutLon float64     `json:"drop_out_lon"`
+	CreatedAt  time.Time   `json:"created_at"`
+	UpdatedAt  time.Time   `json:"updated_at"`
 }
 
 // OrderStatus is a ENUM for orderStatus in Orders Table
