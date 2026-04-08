ed16c4078731fa5e53835d5e9d8b65ff9bd958f1
Alex Popov <ukiquee@email.com>
2026-04-08 12:31:40 +0300

    feat: add SaveLocation repository func

---
+++ internal/features/locations/repository/location.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/ukique/taxi-service/internal/models"
+)
+
+func SaveLocation(ctx context.Context, conn *pgx.Conn, orderBody models.OrderCoordinateEvent) error {
+	sqlQuery := `
+	INSERT INTO driver_locations (order_id, driver_id, lat,lon, generated_time)
+	VALUES ($1,$2,$3,$4,$5);
+`
+	_, err := conn.Exec(ctx, sqlQuery, orderBody.Order.ID, orderBody.DriverID, orderBody.Coordinates.Lat, orderBody.Coordinates.Lon, orderBody.Coordinates.GeneratedTime)
+	if err != nil {
+		return err
+	}
+	return nil
+}
