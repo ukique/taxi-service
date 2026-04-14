62a91bd9f28e668cfbd965016c964db92a11a6d9
Alex Popov <ukiquee@email.com>
2026-04-14 11:44:58 +0300

    refactor: add handler struct to every GIN handler

--- internal/features/user/transport/authenticationHandler.go
+++ internal/features/user/transport/authenticationHandler.go
@@ -11,28 +11,35 @@ import (
 	"github.com/ukique/taxi-service/internal/models"
 )
 
-func AuthenticationUserHandler(pool *pgxpool.Pool, secretKey string) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		var user models.User
-		if err := c.ShouldBindJSON(&user); err != nil {
-			log.Println("fail to read JSON:", err)
-			c.JSON(http.StatusBadRequest, gin.H{"message": "fail to read data"})
-			return
-		}
-		isValid := service.VerifyUserCredentials(c.Request.Context(), pool, user.Email, user.Username, user.Password)
-		if isValid {
-			tokenJWT, err := middleware.GenerateJWT(user, secretKey)
-			if err != nil {
-				log.Println("fail to sign key:", err)
-				c.JSON(http.StatusInternalServerError, gin.H{"message": "server error"})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{"token": tokenJWT})
-		} else {
-			log.Println("user data isn't valid")
-			c.JSON(http.StatusBadRequest, gin.H{"message": "data isn't valid"})
-			return
-		}
+type AuthHandler struct {
+	pool      *pgxpool.Pool
+	secretKey string
+}
 
+func NewAuthUserHandler(pool *pgxpool.Pool, secretKey string) *AuthHandler {
+	return &AuthHandler{pool: pool, secretKey: secretKey}
+}
+
+func (h *AuthHandler) AuthenticationUserHandler(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		log.Println("fail to read JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "fail to read data"})
+		return
 	}
+	isValid := service.VerifyUserCredentials(c.Request.Context(), h.pool, user.Email, user.Username, user.Password)
+	if isValid {
+		tokenJWT, err := middleware.GenerateJWT(user, h.secretKey)
+		if err != nil {
+			log.Println("fail to sign key:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "server error"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"token": tokenJWT})
+	} else {
+		log.Println("user data isn't valid")
+		c.JSON(http.StatusBadRequest, gin.H{"message": "data isn't valid"})
+		return
+	}
+
 }
